073e230939e359fbc55b04832ecfa0a469b444d0
Julia <grigorchukj@gmail.com>
2019-10-28 01:05:38 +0200

    Add build to repo.

---
+++ build/precache-manifest.87cd8f90b5327afbf21129649177e728.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "156989b5b19e183dfcd7116850c548e5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cc679cb373ad9fbc3025",
+    "url": "/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "6ab79ae60fe37505d0e8",
+    "url": "/static/css/main.24bf2ac4.chunk.css"
+  },
+  {
+    "revision": "cc679cb373ad9fbc3025",
+    "url": "/static/js/2.46e86cf1.chunk.js"
+  },
+  {
+    "revision": "6ab79ae60fe37505d0e8",
+    "url": "/static/js/main.4685e29b.chunk.js"
+  },
+  {
+    "revision": "f1c6019deaec6f4a9d0b",
+    "url": "/static/js/runtime-main.11bc70d3.js"
+  }
+]);
